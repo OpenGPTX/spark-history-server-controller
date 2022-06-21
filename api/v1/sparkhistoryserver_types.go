269c31895d8de4459f47ff566016d69611da940e
tikr7 <tim.krause@alexanderthamm.com>
2022-06-21 13:15:17 +0200

    boilertemplate

---
+++ api/v1/sparkhistoryserver_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SparkHistoryServerSpec defines the desired state of SparkHistoryServer
+type SparkHistoryServerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SparkHistoryServer. Edit sparkhistoryserver_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SparkHistoryServerStatus defines the observed state of SparkHistoryServer
+type SparkHistoryServerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// SparkHistoryServer is the Schema for the sparkhistoryservers API
+type SparkHistoryServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SparkHistoryServerSpec   `json:"spec,omitempty"`
+	Status SparkHistoryServerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SparkHistoryServerList contains a list of SparkHistoryServer
+type SparkHistoryServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SparkHistoryServer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SparkHistoryServer{}, &SparkHistoryServerList{})
+}
