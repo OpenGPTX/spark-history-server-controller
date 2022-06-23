e48ac3f8c5a3d47c4d669f69af140ed4309707a7
tikr7 <tim.krause@alexanderthamm.com>
2022-06-23 17:13:19 +0200

    first deployment done

--- controllers/sparkhistoryserver_controller.go
+++ controllers/sparkhistoryserver_controller.go
@@ -18,13 +18,22 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
 	"time"
 
 	"github.com/go-logr/logr"
+	reconcilehelper "github.com/kubeflow/kubeflow/components/common/reconcilehelper"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -33,6 +42,10 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	kubricksv1 "kubricks.io/sparkhistoryserver/api/v1"
 )
+
+const DefaultServingPort = 18080
+const AnnotationRewriteURI = "notebooks.kubeflow.org/http-rewrite-uri"
+const AnnotationHeadersRequestSet = "notebooks.kubeflow.org/http-headers-request-set"
 
 // SparkHistoryServerReconciler reconciles a SparkHistoryServer object
 type SparkHistoryServerReconciler struct {
@@ -44,6 +57,8 @@ type SparkHistoryServerReconciler struct {
 //+kubebuilder:rbac:groups=kubricks.kubricks.io,resources=sparkhistoryservers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kubricks.kubricks.io,resources=sparkhistoryservers/finalizers,verbs=update
 //+kubebuilder:rbac:groups=api.core.v1,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="networking.istio.io",resources=virtualservices,verbs="*"
+//+kubebuilder:rbac:groups="networking.istio.io",resources=envoyfilters,verbs="*"
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -100,6 +115,18 @@ func (r *SparkHistoryServerReconciler) Reconcile(ctx context.Context, req ctrl.R
 		return r.CreateService(ctx, req, sparkhistoryserver, log)
 	}
 
+	// Reconcile virtual service.
+	err := r.reconcileVirtualService(&sparkhistoryserver, log)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Reconcile envoy filter.
+	err = r.reconcileEnvoyFilter(req, &sparkhistoryserver, log)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
 }
 
@@ -112,19 +139,8 @@ func (r *SparkHistoryServerReconciler) SetupWithManager(mgr ctrl.Manager) error 
 
 // CreateDeployment creates the deployment in the cluster.
 func (r *SparkHistoryServerReconciler) CreateDeployment(ctx context.Context, req ctrl.Request, sparkhistoryserver kubricksv1.SparkHistoryServer, log logr.Logger) (ctrl.Result, error) {
-	//startcommand := "export SPARK_HISTORY_OPTS=\"$SPARK_HISTORY_OPTS -Dspark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem -Dspark.ui.proxyBase=/sparkhistory/salil-mishra -Dspark.ui.reverseProxy=true -Dspark.ui.reverseProxyUrl=https://kubeflow.at.onplural.sh/sparkhistory/salil-mishra -Dspark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.WebIdentityTokenCredentialsProvider -Dspark.history.fs.logDirectory=s3a://at-plural-sh-at-onplural-sh-kubeflow-pipelines/pipelines/salil-mishra/history\"; /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer;"
 
-	startcommand := "export SPARK_HISTORY_OPTS=\"$SPARK_HISTORY_OPTS"
-	startcommand += " -Dspark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem"
-	//	startcommand += " -Dspark.ui.proxyBase=/sparkhistory/salil-mishra"
-	//	startcommand += " -Dspark.ui.reverseProxy=true"
-	//	startcommand += " Dspark.ui.reverseProxyUrl=https://kubeflow.at.onplural.sh/sparkhistory/salil-mishra"
-	//	startcommand += " -Dspark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.WebIdentityTokenCredentialsProvider"
-	startcommand += " -Dspark.hadoop.fs.s3a.access.key=AKIA4SDN3Y37NY4UADGE"
-	startcommand += " -Dspark.hadoop.fs.s3a.secret.key=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"
-	startcommand += " -Dspark.history.fs.logDirectory=s3a://tims-delta-lake/history\";"
-	//startcommand += " -Dspark.history.fs.logDirectory=s3a://at-plural-sh-at-onplural-sh-kubeflow-pipelines/pipelines/salil-mishra/history\";"
-	startcommand += " /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer;"
+	var inlineCode = "export SPARK_HISTORY_OPTS=\"$SPARK_HISTORY_OPTS \\\n  -Dspark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \\\n  -Dspark.ui.proxyBase=/sparkhistory/" + req.Namespace + " \\\n  -Dspark.ui.reverseProxy=true \\\n  -Dspark.ui.reverseProxyUrl=https://kubeflow.at.onplural.sh/sparkhistory/" + req.Namespace + " \\\n  -Dspark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.WebIdentityTokenCredentialsProvider \\\n  -Dspark.history.fs.logDirectory=s3a://at-plural-sh-at-onplural-sh-kubeflow-pipelines/pipelines/" + req.Namespace + "/history\";\n/opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer;\n"
 
 	var sparkhistoryserverDeployment *appsv1.Deployment
 	sparkhistoryserverDeployment = &appsv1.Deployment{
@@ -150,8 +166,6 @@ func (r *SparkHistoryServerReconciler) CreateDeployment(ctx context.Context, req
 						"app.kubernetes.io/name":     req.Name,
 						"app.kubernetes.io/instance": req.Namespace,
 					},
-					Name:      req.Name,
-					Namespace: req.Namespace,
 				},
 				Spec: corev1.PodSpec{
 					ServiceAccountName: "default-editor",
@@ -168,6 +182,7 @@ func (r *SparkHistoryServerReconciler) CreateDeployment(ctx context.Context, req
 							Ports: []corev1.ContainerPort{
 								{
 									ContainerPort: 18080, //sparkhistoryserver.Spec.Port,
+									Name:          "historyport",
 								},
 							},
 							ImagePullPolicy: corev1.PullIfNotPresent,
@@ -179,7 +194,7 @@ func (r *SparkHistoryServerReconciler) CreateDeployment(ctx context.Context, req
 							Command: []string{
 								"/bin/sh",
 								"-c",
-								startcommand,
+								string(inlineCode),
 							},
 						},
 					},
@@ -224,7 +239,7 @@ func (r *SparkHistoryServerReconciler) CreateService(ctx context.Context, req ct
 				{
 					Name:       "http",
 					Port:       18080,
-					TargetPort: intstr.FromInt(18080), //TODO historyport
+					TargetPort: intstr.FromString("historyport"),
 					Protocol:   "TCP",
 				},
 			},
@@ -252,4 +267,270 @@ func (r *SparkHistoryServerReconciler) RemoveService(ctx context.Context, servic
 	}
 	log.V(1).Info("Removed sparkhistoryserver service for sparkhistoryserver run", "sparkhistoryserverPod", name)
 	return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+}
+
+func virtualServiceName(kfName string, namespace string) string {
+	return fmt.Sprintf("notebook-%s-%s", namespace, kfName)
+}
+
+func generateVirtualService(instance *kubricksv1.SparkHistoryServer) (*unstructured.Unstructured, error) {
+	name := instance.Name
+	namespace := instance.Namespace
+	clusterDomain := "cluster.local"
+	prefix := fmt.Sprintf("/sparkhistory/%s", namespace)
+
+	// unpack annotations from Notebook resource
+	annotations := make(map[string]string)
+	for k, v := range instance.ObjectMeta.Annotations {
+		annotations[k] = v
+	}
+
+	rewrite := fmt.Sprintf("/")
+	// If AnnotationRewriteURI is present, use this value for "rewrite"
+	if _, ok := annotations[AnnotationRewriteURI]; ok && len(annotations[AnnotationRewriteURI]) > 0 {
+		rewrite = annotations[AnnotationRewriteURI]
+	}
+
+	if clusterDomainFromEnv, ok := os.LookupEnv("CLUSTER_DOMAIN"); ok {
+		clusterDomain = clusterDomainFromEnv
+	}
+	service := fmt.Sprintf("%s.%s.svc.%s", name, namespace, clusterDomain)
+
+	vsvc := &unstructured.Unstructured{}
+	vsvc.SetAPIVersion("networking.istio.io/v1alpha3")
+	vsvc.SetKind("VirtualService")
+	vsvc.SetName(virtualServiceName(name, namespace))
+	vsvc.SetNamespace(namespace)
+	if err := unstructured.SetNestedStringSlice(vsvc.Object, []string{"*"}, "spec", "hosts"); err != nil {
+		return nil, fmt.Errorf("Set .spec.hosts error: %v", err)
+	}
+
+	istioGateway := os.Getenv("ISTIO_GATEWAY")
+	if len(istioGateway) == 0 {
+		istioGateway = "kubeflow/kubeflow-gateway"
+	}
+	if err := unstructured.SetNestedStringSlice(vsvc.Object, []string{istioGateway},
+		"spec", "gateways"); err != nil {
+		return nil, fmt.Errorf("Set .spec.gateways error: %v", err)
+	}
+
+	headersRequestSet := make(map[string]string)
+	// If AnnotationHeadersRequestSet is present, use its values in "headers.request.set"
+	if _, ok := annotations[AnnotationHeadersRequestSet]; ok && len(annotations[AnnotationHeadersRequestSet]) > 0 {
+		requestHeadersBytes := []byte(annotations[AnnotationHeadersRequestSet])
+		if err := json.Unmarshal(requestHeadersBytes, &headersRequestSet); err != nil {
+			// if JSON decoding fails, set an empty map
+			headersRequestSet = make(map[string]string)
+		}
+	}
+	// cast from map[string]string, as SetNestedSlice needs map[string]interface{}
+	headersRequestSetInterface := make(map[string]interface{})
+	for key, element := range headersRequestSet {
+		headersRequestSetInterface[key] = element
+	}
+
+	// the http section of the istio VirtualService spec
+	http := []interface{}{
+		map[string]interface{}{
+			"match": []interface{}{
+				map[string]interface{}{
+					"uri": map[string]interface{}{
+						"prefix": prefix + "/",
+					},
+				},
+				map[string]interface{}{
+					"uri": map[string]interface{}{
+						"prefix": prefix,
+					},
+				},
+			},
+			"rewrite": map[string]interface{}{
+				"uri": rewrite,
+			},
+			"route": []interface{}{
+				map[string]interface{}{
+					"destination": map[string]interface{}{
+						"host": service,
+						"port": map[string]interface{}{
+							"number": int64(DefaultServingPort),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// add http section to istio VirtualService spec
+	if err := unstructured.SetNestedSlice(vsvc.Object, http, "spec", "http"); err != nil {
+		return nil, fmt.Errorf("Set .spec.http error: %v", err)
+	}
+
+	return vsvc, nil
+
+}
+
+func (r *SparkHistoryServerReconciler) reconcileVirtualService(instance *kubricksv1.SparkHistoryServer, log logr.Logger) error {
+	//log := r.Log.WithValues("notebook", instance.Namespace)
+	virtualService, err := generateVirtualService(instance)
+	if err := ctrl.SetControllerReference(instance, virtualService, r.Scheme); err != nil {
+		return err
+	}
+	// Check if the virtual service already exists.
+	foundVirtual := &unstructured.Unstructured{}
+	justCreated := false
+	foundVirtual.SetAPIVersion("networking.istio.io/v1alpha3")
+	foundVirtual.SetKind("VirtualService")
+	err = r.Get(context.TODO(), types.NamespacedName{Name: virtualServiceName(instance.Name,
+		instance.Namespace), Namespace: instance.Namespace}, foundVirtual)
+	if err != nil && apierrs.IsNotFound(err) {
+		log.Info("Creating virtual service", "namespace", instance.Namespace, "name",
+			virtualServiceName(instance.Name, instance.Namespace))
+		err = r.Create(context.TODO(), virtualService)
+		justCreated = true
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	if !justCreated && reconcilehelper.CopyVirtualService(virtualService, foundVirtual) {
+		log.Info("Updating virtual service", "namespace", instance.Namespace, "name",
+			virtualServiceName(instance.Name, instance.Namespace))
+		err = r.Update(context.TODO(), foundVirtual)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func envoyFilterName(kfName string, namespace string) string {
+	return fmt.Sprintf("notebook-%s-%s", namespace, kfName)
+}
+
+func generateEnvoyFilter(req ctrl.Request, instance *kubricksv1.SparkHistoryServer, log logr.Logger) (*unstructured.Unstructured, error) {
+	name := instance.Name
+	namespace := instance.Namespace
+
+	vsvc := &unstructured.Unstructured{}
+	vsvc.SetAPIVersion("networking.istio.io/v1alpha3")
+	vsvc.SetKind("EnvoyFilter")
+	vsvc.SetName(envoyFilterName(name, namespace))
+	vsvc.SetNamespace(namespace)
+	if err := unstructured.SetNestedStringSlice(vsvc.Object, []string{"*"}, "spec", "hosts"); err != nil {
+		return nil, fmt.Errorf("Set .spec.hosts error: %v", err)
+	}
+
+	workloadSelector := map[string]interface{}{
+		"labels": map[string]interface{}{
+			"app.kubernetes.io/name":     req.Name,
+			"app.kubernetes.io/instance": req.Namespace,
+		},
+	}
+	// add workloadSelector section to istio EnvoyFilter spec
+	if err := unstructured.SetNestedField(vsvc.Object, workloadSelector, "spec", "workloadSelector"); err != nil {
+		return nil, fmt.Errorf("Set .spec.http error: %v", err)
+	}
+
+	var inlineCode = "function envoy_on_response(response_handle, context)\n    response_handle:headers():replace(\"location\", \"\");\nend\n"
+
+	configPatches := []interface{}{
+		map[string]interface{}{
+			"applyTo": "HTTP_FILTER",
+			"match": map[string]interface{}{
+				"context": "SIDECAR_INBOUND",
+				"listener": map[string]interface{}{
+					"filterChain": map[string]interface{}{
+						"filter": map[string]interface{}{
+							"name": "envoy.filters.network.http_connection_manager",
+							"subFilter": map[string]interface{}{
+								"name": "envoy.filters.http.router",
+							},
+						},
+					},
+				},
+			},
+			"patch": map[string]interface{}{
+				"operation": "INSERT_BEFORE",
+				"value": map[string]interface{}{
+					"name": "envoy.lua",
+					"typed_config": map[string]interface{}{
+						"@type":      "type.googleapis.com/envoy.extensions.filters.http.lua.v3.Lua",
+						"inlineCode": string(inlineCode),
+					},
+				},
+			},
+		},
+	}
+
+	if err := unstructured.SetNestedSlice(vsvc.Object, configPatches, "spec", "configPatches"); err != nil {
+		return nil, fmt.Errorf("Set .spec.http error: %v", err)
+	}
+
+	return vsvc, nil
+
+}
+
+func (r *SparkHistoryServerReconciler) reconcileEnvoyFilter(req ctrl.Request, instance *kubricksv1.SparkHistoryServer, log logr.Logger) error {
+	//log := r.Log.WithValues("notebook", instance.Namespace)
+	envoyFilter, err := generateEnvoyFilter(req, instance, log)
+	if err := ctrl.SetControllerReference(instance, envoyFilter, r.Scheme); err != nil {
+		return err
+	}
+	// Check if the envoy filter already exists.
+	foundEnvoy := &unstructured.Unstructured{}
+	justCreated := false
+	foundEnvoy.SetAPIVersion("networking.istio.io/v1alpha3")
+	foundEnvoy.SetKind("EnvoyFilter")
+	err = r.Get(context.TODO(), types.NamespacedName{Name: envoyFilterName(instance.Name,
+		instance.Namespace), Namespace: instance.Namespace}, foundEnvoy)
+	if err != nil && apierrs.IsNotFound(err) {
+		log.Info("Creating envoy filter", "namespace", instance.Namespace, "name",
+			envoyFilterName(instance.Name, instance.Namespace))
+		err = r.Create(context.TODO(), envoyFilter)
+		justCreated = true
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	if !justCreated && CopyEnvoyFilter(envoyFilter, foundEnvoy) {
+		log.Info("Updating envoy filter", "namespace", instance.Namespace, "name",
+			envoyFilterName(instance.Name, instance.Namespace))
+		err = r.Update(context.TODO(), foundEnvoy)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//https://github.com/kubeflow/kubeflow/blob/7f4231de77ea/components/common/reconcilehelper/util.go#L199
+// Copy configuration related fields to another instance and returns true if there
+// is a diff and thus needs to update.
+func CopyEnvoyFilter(from, to *unstructured.Unstructured) bool {
+	fromSpec, found, err := unstructured.NestedMap(from.Object, "spec")
+	if !found {
+		return false
+	}
+	if err != nil {
+		return false
+	}
+
+	toSpec, found, err := unstructured.NestedMap(to.Object, "spec")
+	if !found || err != nil {
+		unstructured.SetNestedMap(to.Object, fromSpec, "spec")
+		return true
+	}
+
+	requiresUpdate := !reflect.DeepEqual(fromSpec, toSpec)
+	if requiresUpdate {
+		unstructured.SetNestedMap(to.Object, fromSpec, "spec")
+	}
+	return requiresUpdate
 }
